52376024052d428f3b8678ebe575188e030f0329
sheema <sheemashamilfarook@gmail.com>
2022-11-28 15:56:49 +0300

    done

--- grade.js
+++ grade.js
@@ -1,19 +1,31 @@
-Grade
-let score = 100;
-let Grade;
-
-if(score >= 90){
-    Grade ='A'
-}
-else if(score >= 80){
-    Grade ='B'
-}
-else if(score => 70){
-    Grade ='C'
-}
-else if(score >= 70){
-    Grade ='D'
-}
-else{
-    Grade ='F'
-}
+function studentGrading(){ 
+    let score =prompt("Enter score") 
+    let grade; 
+    // const msg= function(grade){ 
+        // return `you Grade is'{grade}` 
+    // } 
+    if(score >=0 && score <= 100){ 
+        if(score >= 79){ 
+            grade ='A' 
+        } 
+        else if(score >59 && score <79){ 
+            grade ='B' 
+        } 
+        else if(score >39 && score <49){ 
+            grade ='C' 
+        } 
+        else if(score <40){ 
+            grade ='D' 
+        } 
+        else{ 
+            grade ='E' 
+        } 
+         
+    document.body.innerHTML=`you Grade is ${grade}` 
+    } 
+    else { 
+        document.body.innerHTML="invalid Score" 
+    } 
+    } 
+    studentGrading()
+   
